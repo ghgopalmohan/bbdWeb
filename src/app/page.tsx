b6822a615a60470878c8bc56794f43d1b2f7d68d
Rishi Gh <rishigh001@gmail.com>
2025-06-14 13:17:53 +0000

    remove testimonals section completely

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import AboutSection from '@/components/sections/about-section';
 import DesignProcessSection from '@/components/sections/design-process-section';
 import PortfolioSection from '@/components/sections/portfolio-section';
 import ServicesSection from '@/components/sections/services-section';
-import TestimonialsSection from '@/components/sections/testimonials-section';
 import ContactSection from '@/components/sections/contact-section';
 
 export default function HomePage() {
@@ -21,7 +20,6 @@ export default function HomePage() {
         <ServicesSection />
         <PortfolioSection />
         <ClientsSection /> 
-        <TestimonialsSection />
         <ContactSection />
       </main>
       <Footer />
